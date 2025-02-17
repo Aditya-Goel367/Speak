5041fd1332a8f88219b31d44750f81ff0b787a44
Aditya-Goel367 <adigoel833@gmail.com>
2025-02-18 00:24:30 +0530

    Updated app.js with new features/fixes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,9 @@
-const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
+const tailwindcss = require("tailwindcss");
 
 module.exports = {
   plugins: [
-    require("@tailwindcss/postcss")(),
-    tailwindcss(),
+    tailwindcss(), // Use main Tailwind package
     autoprefixer(),
   ],
 };
